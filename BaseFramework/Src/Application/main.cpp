f7af4526508e2a5eedd00f58871c088365a4fa0f
zushijyunitirou <kd1360051@st.kobedenshi.ac.jp>
2024-04-17 10:30:52 +0900

    絶対消すやつ
    黒歴史

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,6 +221,9 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
+
+	//絶対消すぞ
+	m_test = 5;
 	return true;
 }
 
